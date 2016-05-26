2579784af9ea1676521f8a8fe1776090d5d67127
LiuchangDuan <870121425@qq.com>
2016-05-26 15:41:57 +0800

    动态加载和静态加载

--- src/com/example/fragmentdemo/MainActivity.java
+++ src/com/example/fragmentdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fragmentdemo;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
@@ -22,8 +25,16 @@ public class MainActivity extends Activity implements OnCheckedChangeListener {
 	public void onCheckedChanged(RadioGroup group, int checkedId) {
 		switch (checkedId) {
 		case R.id.first:
+			Intent intent = new Intent(this, MainActivity2.class);
+			startActivity(intent);
 			break;
 		case R.id.second:
+			MyFragment2 fragment2 = new MyFragment2();
+			FragmentManager fragmentManager = getFragmentManager();
+			FragmentTransaction beginTransaction = fragmentManager.beginTransaction();
+			beginTransaction.add(R.id.frame, fragment2);
+			beginTransaction.addToBackStack(null);
+			beginTransaction.commit();
 			break;
 		case R.id.third:
 			break;
